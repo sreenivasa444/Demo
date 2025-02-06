50d1712a0043b24777ec5ceee2393f56e7d589a1
sreenivasa444 <sreenivasa.reddy@simplify3x.com>
2025-02-06 12:47:38 +0530

    Add Files

---
+++ Demotest.java
@@ -0,0 +1,11 @@
+public class AddNumbers {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 20;
+        
+        // Adding two numbers
+        int sum = num1 + num2;
+        
+        System.out.println("Sum of " + num1 + " and " + num2 + " is: " + sum);
+    }
+}
